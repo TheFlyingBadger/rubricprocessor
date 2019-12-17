8db26db44e64d620c532b5223883f52ca087c6e5
badger <jon@badger.shoes>
2019-12-17 09:05:40 +0800

    add "RubricProcessor.py" file  to show example of calling the processor

---
+++ RubricProcessor.py
@@ -0,0 +1,11 @@
+import rubricprocessor
+
+# rubricprocessor.guiLaunch(zipfile         = "D:/Python/Suzie/RubricProcessor/input/rubric/ArchiveFile_ACC3201.2019.2.ONCAMPUS_OFFCAMPUS_20191126085012.ZIP"
+                         # ,gradecentreCSV  = "D:/Python/Suzie/RubricProcessor/input/gc/gc_ACC3201_2019_2_ONCAMPUS_OFFCA.csv"
+                         # )
+
+rubricprocessor.runFromCLI()
+
+# rubricprocessor.processBatch (control      = r'.\batch.csv'
+                             # ,outputFolder = r"D:\Python\Suzie\RubricProcessor\output"
+                             # )
